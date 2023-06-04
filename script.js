54fe0e0ff35dfcce90547ac770488bb20d1ac590
Sanchex-22 <David21.cs70@gmail.com>
2023-06-04 11:34:54 -0500

    avances

---
+++ script.js
@@ -0,0 +1,21 @@
+const wrappper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    wrappper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrappper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrappper.classList.add('active-popud');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrappper.classList.remove('active-popud');
+});
