8b61de0394790b263346093bccc27eb06aeffeb1
Sanchex-22 <David21.cs70@gmail.com>
2023-06-07 00:37:12 -0500

    xd

--- js/server.js
+++ js/server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
 
-const port = 3000; // Puedes cambiar el número de puerto si es necesario
+// const port = 3000; // Puedes cambiar el número de puerto si es necesario
 
-// Ruta para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+// // Ruta para servir archivos estáticos
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para manejar todas las solicitudes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// // Ruta para manejar todas las solicitudes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor web iniciado en http://localhost:${port}`);
-});
+// // Iniciar el servidor
+// app.listen(port, () => {
+//   // console.log(`Servidor web iniciado en http://localhost:${port}`);
+//   console.log(`Servidor web iniciado en  https://zeyn20.github.io/pagina-web/`);
+// });
